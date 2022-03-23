189363a3aeb29f81ad2f4a0a31c94c7226fda4fa
Jessica <jessicanevesmachado@gmail.com>
2022-03-22 21:36:44 -0300

    test: 💍 add sum test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe("Sum", () => {
+  it("should return 4", () => {
+    expect(2 + 3).toBe(4);
+  });
 });
